d8338210ca260f59081f27dfb6370941fc08840b
Brian Jenkins <brianjenkins94@gmail.com>
2023-02-03 17:28:01 -0500

    Beginning of `find`

--- packages/find/find.ts
+++ packages/find/find.ts
@@ -1,170 +1,72 @@
-import { readdir, stat } from "fs/promises";
-import { sep as _sep } from "path";
+/* eslint @typescript-eslint/typedef: "error" */
+/* eslint @typescript-eslint/explicit-function-return-type: "error" */
+/* eslint @typescript-eslint/explicit-module-boundary-types: "error" */
 
-function regexEscape(pattern: string) {
-	// :( https://github.com/tc39/proposal-regex-escaping/issues/37
-	return pattern.replace(/\\/g, "\\\\");
+import { glob, Glob } from "glob";
+import type { GlobCache } from "glob/dist/mjs/readdir";
+import type { GlobOptions } from "glob/dist/mjs/glob";
+
+enum DirentTypes {
+	UV_DIRENT_FILE = 1,
+	UV_DIRENT_DIR = 2
 }
 
-/**
- * @param glob string Shell glob expression.
- * @param matchSep boolean Whether wildcards should match the path component
- * separator (e.g. '/' on Unix).
- * @return regex
- */
-function glob2regex(glob: string, matchSep: boolean = false): RegExp {
-	// It seems most (all?) popular glob libraries forbid backslashes as path
-	// separators in globs. Trying to figure out when a backslash is escaping
-	// versus separating is a nightmare. Like them, we presume that every
-	// unescaped forward slash is a path separator.
-	const sep = regexEscape(_sep);
-	const any = matchSep ? "." : `[^${sep}]`;
-	const pattern = glob
-		.replace(/(?<!\\)\//g, sep)
-		.replace(/(?<!\\)\*/g, any + "*")
-		.replace(/(?<!\\)\?/g, any);
-	return new RegExp(`^${pattern}$`);
-}
+class Dirent extends null {
+	public constructor(name, type: DirentTypes) {
+		const dirent = Object.create(new.target.prototype);
+		dirent.name = name;
+		dirent[Symbol("type")] = type;
 
-type Decision = { include: boolean; ascend?: boolean };
-type Filter = (path: Path) => Promise<Decision>;
-
-export const always = async () => ({ "include": true });
-export const never = async () => ({ "include": false });
-
-export const name = (glob: string) => async (path: Path) => ({
-	"include": glob2regex(glob).test(path.name)
-});
-
-export const path = (glob: string) => async (path: Path) => ({
-	"include": glob2regex(glob, true).test(path.toString())
-});
-
-export const regex = (pattern: string | RegExp) => {
-	const r = new RegExp(pattern);
-	return async (path: Path) => ({
-		"include": r.test(path.toString())
-	});
-};
-
-/** A map associating characters, chosen by GNU find, to file types. */
-const METHOD_FOR_TYPE = {
-	"b": "isBlockDevice",
-	"c": "isCharacterDevice",
-	"d": "isDirectory",
-	"f": "isFile",
-	"l": "isSymbolicLink",
-	"p": "isFIFO",
-	"s": "isSocket"
-};
-
-export const type = (c: string) => {
-	console.assert(METHOD_FOR_TYPE.hasOwnProperty(c));
-	return async (path: Path) => ({
-		"include": (await path.stat())[METHOD_FOR_TYPE[c]]()
-	});
-};
-
-export const prune = (filter: Filter) => async (path: Path) => {
-	const { include } = await filter(path);
-	return { "include": include, "ascend": include };
-};
-
-export const not = (filter: Filter) => async (path: Path) => {
-	const { include, ascend } = await filter(path);
-	return { "include": !include, "ascend": ascend };
-};
-
-export const or = (...filters: Filter[]) => async (path: Path) => {
-	for (const filter of filters) {
-		const decision = await filter(path);
-		if (decision.include) {
-			return decision;
-		}
-	}
-	return { "include": false };
-};
-
-export const and = (...filters: Filter[]) => async (path: Path) => {
-	let ascend = false;
-	for (const filter of filters) {
-		const decision = await filter(path);
-		if (!decision.include) {
-			return { "include": false, "ascend": ascend };
-		}
-		// Only read ascend from decisions that include.
-		ascend ||= decision.ascend;
-	}
-	return { "include": true, "ascend": ascend };
-};
-
-export class Path {
-	private status = null;
-
-	public constructor(public steps: string[] = []) { }
-
-	public get depth(): number {
-		return this.steps.length;
-	}
-
-	public get parent(): Path {
-		return new Path(this.steps.slice(0, -1));
-	}
-
-	public get name(): string {
-		return this.steps[this.steps.length - 1];
-	}
-
-	public child(step: string): Path {
-		return new Path([...this.steps, step]);
-	}
-
-	public toString(sep: string = _sep): string {
-		return this.steps.join(sep);
-	}
-
-	public async stat() {
-		if (!this.status) {
-			this.status = await stat(this.toString());
-		}
-		return this.status;
-	}
-
-	public async isDirectory(): Promise<boolean> {
-		return (await this.stat()).isDirectory();
-	}
-
-	public async list(): Promise<Path[]> {
-		return (await readdir(this.toString())).map((child) => this.child(child));
+		// eslint-disable-next-line no-constructor-return
+		return dirent;
 	}
 }
 
-export async function* find(
-	filter: Filter = always,
-	{ start = ".", maxDepth }: { start?: string; maxDepth?: number } = {}
-) {
-	const prefix = start.split(_sep);
-	const stack = [[prefix.pop()]];
-	do {
-		let top = stack.pop();
-		while (top.length > 0) {
-			const step = top.shift();
-			const path = new Path([...prefix, step]);
-			const { include, ascend } = await filter(path);
-			if (include) {
-				yield path;
-			}
-			if (
-				!ascend &&
-				/* `== null` matches `undefined` too. */
-				(maxDepth == null || maxDepth > prefix.length) &&
-				(await path.isDirectory())
-			) {
-				prefix.push(step);
-				stack.push(top);
-				top = await readdir(path.toString());
-			}
+class Find {
+	private glob;
+
+	public constructor(pathOrObject: object | string, pattern: string[] | string, options: GlobOptions = {}) {
+		if (typeof pathOrObject === "string") {
+			options.cwd ??= pathOrObject;
+		} else {
+			options.cwd = "/";
+			options.cache = this.cacheify(pathOrObject);
 		}
-		prefix.pop();
-	} while (stack.length > 0);
+
+		this.glob = new Glob(pattern, options);
+	}
+
+	public exec(functionOrShellCommand): string[] {
+		return this.glob.process();
+	}
+
+	private cacheify(object): GlobCache {
+		const cache = {};
+
+		(function recurse(object): void {
+
+		})(object);
+
+		return cache;
+	}
 }
+
+export function find(pathOrObject: object | string, pattern: string[] | string, options: GlobOptions = {}): Find {
+	const find = new Find(pathOrObject, pattern, options);
+
+	return find;
+}
+
+console.log(glob("**/*.js"));
+
+const object = {
+	"foo": {
+		"bar": {
+			"baz": {}
+		}
+	},
+	"bar": [],
+	"baz": 7
+};
+
+//console.log(find(object, "**/bar"));
