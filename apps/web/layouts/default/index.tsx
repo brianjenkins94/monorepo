96cac470c508c919a51ba5e5fcbdf3bcef7af3d6
Brian Jenkins <brianjenkins94@gmail.com>
2023-02-06 16:38:39 -0500

    Ratatouille becoming its own thing

--- apps/web/layouts/default/index.tsx
+++ apps/web/layouts/default/index.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
 	return (
 		<>
 			<Head>
-				<title>Web</title>
+				<title></title>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet" />
