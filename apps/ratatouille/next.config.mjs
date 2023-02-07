2115289b87b1c6fcfecb9890049e55cfd6321dc6
Brian Jenkins <brianjenkins94@gmail.com>
2023-02-06 20:51:39 -0500

    Update next.config.mjs

--- apps/ratatouille/next.config.mjs
+++ apps/ratatouille/next.config.mjs
@@ -5,7 +5,7 @@ import * as url from "url";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+//
 // TODO: Extract to `./config`
 const __root = __dirname;
 
